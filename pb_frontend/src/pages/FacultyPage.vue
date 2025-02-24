8a57b1e38cb25e9df84b9e483b89d0f872db438a
ruddnev <nikita-rudnev@ya.ru>
2025-02-24 16:58:30 +0300

    frontend-new v1

--- pb_frontend/src/pages/FacultyPage.vue
+++ pb_frontend/src/pages/FacultyPage.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
+  <div class="faculty-page">
     <app-header/>
+    <h2>Выберите факультет: </h2>
     <form action="/api/faculty" class="form-container">
       <div class="radio-container">
         <label class="radio-label">
-          <input type="radio" name="faculty" value="KTU" required> КТУ
+        <input type="radio" name="faculty" value="KTU" required> КТУ
         </label>
         <label class="radio-label">
           <input type="radio" name="faculty" value="TINT"> ТИНТ
@@ -30,20 +31,74 @@ import AppHeader from "@/components/AppHeader.vue";
 export default {
   components: {AppHeader},
   mounted() {
-    document.title='MEGA PixelBattle: Faculty'
+    document.title='Choose your faculty'
   }
 }
 </script>
 
 <style scoped>
+.faculty-page {
+  
+  font-size: 18px;  
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  box-sizing: border-box;
+  position: fixed;
+  background-image: url("@/img/paper-bg.jpg");
+  background-size: cover;
+  transition: all 0.5s ease-in-out;
+  text-align: center;
+  position: absolute;
+  width: 100dvw;
+  height: 100dvh;
+  left: 0;
+  top: 0;
+  padding-inline: 10%;
+}
+
+input[type="radio"] {
+  /* display: none; */
+  /* appearance: none; */
+  background-color: #134293;
+}
+/* 
+input[type="radio"] + label {
+  border-color: black;
+  border-radius: 5px;
+} */
+
+/* label:before {
+	content: "";
+	display: inline-block;
+	width: 17px;
+	height: 18px;
+	position: absolute;
+	left: -40px;
+	bottom: -2px;
+	background-color: transparent;
+  border: 2px solid #134293;
+  border-radius: 20%;
+} */
+
+label {
+  color: #134293;
+  position: relative;
+}
+
+input[type="radio"]:checked ~ label {
+  background-color: #bbb;
+}
+
 .form-container {
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
   /* justify-content: center; */
   margin-top: 20px; /* Adjust this value as needed */
   transition: background 1s;
-  filter: drop-shadow(1px 1px grey);
+  /* filter: drop-shadow(1px 1px grey); */
   font-family: Arial, sans-serif;
 }
 
@@ -59,19 +114,32 @@ export default {
 }
 
 .send-button {
-  padding: 10px 20px;
-  /* margin-top: 20px; */
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  border-radius: 5px;
+  border: 0;
+  
+  background-image: url("@/img/button.png");
   cursor: pointer;
-  transition: background-color 0.3s ease;
-  filter: drop-shadow(1px 1px grey);
-  font-family: Arial, sans-serif;
+  transition: all .1s ease-out;
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  height: 40px;
+  width: 400px;
+  background-color: transparent;
+  color: #134293;
 }
 
 .send-button:hover {
-  background-color: #0056b3;
+  color: #134293;
+}
+
+* {
+  font-family: 'Calligrapher';
+  text-align: center;
+}
+
+h2 {
+  margin: 0;
+  margin-top: 30px;
+  color: #134293;
 }
 </style>
--- pb_frontend/src/pages/LoginPage.vue
+++ pb_frontend/src/pages/LoginPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="login-page">
     <app-header/>
     <v-k-login-btn/>
   </div>
@@ -13,7 +13,7 @@ import VKLoginBtn from "@/components/VKLoginBtn.vue";
 export default {
   components: {AppHeader, VKLoginBtn},
   beforeCreate() {
-    document.title='MEGA PixelBattle: Login'
+    document.title='Login to Pixelbattle'
   }
 }
 
@@ -22,4 +22,23 @@ export default {
 
 <style scoped>
 
+.login-page {
+  
+  font-size: 18px;  
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  box-sizing: border-box;
+  position: fixed;
+  background-image: url("@/img/paper-bg.jpg");
+  background-size: cover;
+  transition: all 0.5s ease-in-out;
+  text-align: center;
+  position: absolute;
+  width: 100dvw;
+  height: 100dvh;
+  left: 0;
+  top: 0;
+  padding-inline: 10%;
+}
 </style>
